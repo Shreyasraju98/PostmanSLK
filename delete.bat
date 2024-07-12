36b65854598e525ccc1c9353d30d72da37f01a5c
slk <slk@WSLKCMP4FTR2-11.SLKSOFT.COM>
2024-07-12 11:24:28 +0530

    Pipeline

--- delete.bat
+++ delete.bat
@@ -1,7 +1,23 @@
-bat '''cd..
+pipeline {
+    agent any
+
+    stages {
+        stage('Clean') {
+            steps {
+                echo 'Cleaned'
+                bat '''cd..
                        cd..
                        cd..
                        cd..
                        cd..
                        cd C:\\Jenkins
                        rmdir temp1'''
+            }
+        }
+        stage('BYE') {
+            steps {
+                echo 'Bye World'
+            }
+        }
+    }
+}
